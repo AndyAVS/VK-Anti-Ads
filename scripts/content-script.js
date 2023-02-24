d2eabc7fb284c2fdbdcf55dfab4d66d6640134bc
DESKTOP-1M9D9FV\Shaman <deep4rest@ya.ru>
2023-02-24 17:03:19 +0900

    icons

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -5,25 +5,30 @@ const adsText = 'Реклама в сообществе';
 const observeNode = document;
 //-----------------------------------------------------------------------------
 new MutationObserver(() => {
+
   RemoveBlock();
+
 }).observe(observeNode, { subtree: true, childList: true });
 //-----------------------------------------------------------------------------
 function RemoveBlock() {
-  let ads = getElementsByText(adsText, 'span');
+  
+  let ads = getElementsByText(observeNode, adsText, 'span');
   ads.forEach(adSpan => {
-    console.log('found vk ad');
     let adBlock = adSpan.parentNode.parentNode.parentNode.parentNode;
     if (adBlock) {
       adBlock.style.display = 'none';
+      console.log('[VK Anti-Ads]: found and removed vk ad post');
     }
   });
 }
 //-----------------------------------------------------------------------------
-function getElementsByText(str, tag) {
+function getElementsByText(startNode, str, tag) {
+
   return Array.prototype.slice.call(
-    observeNode.getElementsByTagName(tag)).filter(el => el.textContent.trim() === str.trim()
+    startNode.getElementsByTagName(tag)).filter(el => el.textContent.trim() === str.trim()
     );
+
 }
 //-----------------------------------------------------------------------------
-//-----------------------------------------------------------------------------
+//------------------------------END OF FILE------------------------------------
 //-----------------------------------------------------------------------------
